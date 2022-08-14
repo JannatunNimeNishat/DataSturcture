42fd3fdac672d046e007236a149f5464b1c27b9e
JannatunNimeNishat <nishatnime100@gmail.com>
2022-08-14 13:01:37 +0600

    delete operation
    simple delete operations

--- deleteOperationonLinkList.cpp
+++ deleteOperationonLinkList.cpp
@@ -1,40 +1,122 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct linklist{
+struct linklist
+{
     int data;
     linklist *nAddress;
-};linklist *head, *nptr, *tptr;
+};
+linklist *head, *nptr, *tptr,*dNode;
 
 int main()
 {
     cout<<"\t\tDeletation operation: \n"<<endl;
     head = NULL;
-    int n,values;
+    int n,values,c=0;
     cout<<"Enter the number of node u want: "<<endl;
     cin>>n;
-    cout<<"Enter the values: "<<endl;
-
-    for(int i=0;i<n;i++)
+    if(n<=0)
     {
+        cout<<"Invalid size";
 
-        cin>>values;
+    }
+    else
+    {
+        cout<<"Enter the values: "<<endl;
 
-         nptr = new (linklist);
-        nptr->nAddress = NULL;
-        nptr->data = values;
-
-        if(head==NULL)
+        for(int i=0; i<n; i++)
         {
-            head = nptr;
-            tptr= nptr;
+
+            cin>>values;
+
+            nptr = new (linklist);
+            nptr->nAddress = NULL;
+            nptr->data = values;
+
+            if(head==NULL)
+            {
+                head = nptr;
+                tptr= nptr;
+            }
+            else
+            {
+                tptr->nAddress= nptr;
+                tptr = nptr;
+            }
+            c++;
+        }
+    }
+    cout<<"\t\t\tDelete operation\n"<<endl;
+    cout<<"press 1. to delete the first node\npress 2. to delete the lase node\npress 3. to delete from a specific possition \n"<<endl;
+    int choice=0;
+    cout<<"choice: ";
+    cin>>choice;
+
+    switch(choice)
+    {
+    case 1:
+        //delete at very first
+        tptr = head;
+        head = tptr->nAddress;
+        break;
+    case 2:
+        //delete at very last
+
+        tptr = head;
+        while(tptr->nAddress!=NULL)
+        {
+
+            dNode= tptr;
+            tptr= tptr->nAddress;
+        }
+
+        dNode->nAddress =NULL;
+        break;
+    case 3:
+        //delete after a given position
+        int dPos=0,flag=1;
+        cout<<"Enter the position: ";
+        cin>>dPos;
+
+        if( dPos<=0 ||dPos>c)
+        {
+            cout<<"Invalid possition"<<endl;
+        }
+        else if(dPos==1) // remove the 1st node
+        {
+            tptr = head;
+            head = tptr->nAddress;
         }
         else
         {
-            tptr->nAddress= nptr;
-            tptr = nptr;
+            tptr = head;
+            while(flag<dPos-1)
+            {
+
+                tptr = tptr->nAddress;
+                flag++;
+
+            }
+            dNode= tptr->nAddress;
+            /*
+            note:
+            dNote=tptr
+            means they are the same node
+
+            dNote = tptr->nAddress
+            means dNote now points to the next node addrress of tptr node.
+            */
+
+            // cout<<"raw: "<<dNode->data<<endl;
+            //cout<<"raw: "<<tptr->data<<endl;
+            tptr->nAddress = dNode->nAddress;
+            free(dNode);
         }
+        break;
+
+
     }
+
 
 //printing the link list
     tptr = head;
@@ -44,8 +126,5 @@ int main()
         cout<<tptr->data<<" ";
         tptr = tptr->nAddress;
     }
-
-
-
 
 }
